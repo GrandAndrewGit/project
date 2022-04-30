805d1d83c61c8c718941c9c6ae769659b699d6de
Grand <grand.edocs@gmail.com>
2022-04-30 11:26:56 +0300

    first app

--- js/script.js
+++ js/script.js
@@ -8,4 +8,25 @@
 
 // console.log(obj.name + '   ' + obj.age)
 
-alert('Hello');
+// alert('Hello');
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних промотренных фильмов', '');
+const aRang = +prompt('Дайте ему оценку', '');
+
+const b = prompt('Один из последних промотренных фильмов', '');
+const bRang = +prompt('Дайте ему оценку', '');
+
+personalMovieDB.movies[a] = aRang;
+personalMovieDB.movies[b] = bRang;
+
+console.log(personalMovieDB)
